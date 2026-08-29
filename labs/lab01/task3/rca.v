4bb9c1e422c2f58bd4b383862f55a9cf74787d22
Shivam Ghadge <f20251368@pilani.bits-pilani.ac.in>
2026-08-29 10:56:26 +0000

    lab01 task3

--- labs/lab01/task3/rca.v
+++ labs/lab01/task3/rca.v
@@ -19,7 +19,27 @@ module rca(
 );
 
   wire c1, c2, c3;
-
+  FA_Gate in1(.a(a[0]), .b(b[0]), .cin(cin), .sum(sum[0]), .cout(c1));
+  FA_Gate in2(.a(a[1]), .b(b[1]), .cin(c1), .sum(sum[1]), .cout(c2));
+  FA_Gate in3(.a(a[2]), .b(b[2]), .cin(c2), .sum(sum[2]), .cout(c3));
+  FA_Gate in4(.a(a[3]), .b(b[3]), .cin(c3), .sum(sum[3]), .cout(cout));
   // TODO: your four FA_Gate instances go here.
+
+endmodule
+
+module FA_Gate(
+  input  a,
+  input  b,
+  input  cin,
+  output sum,
+  output cout
+);
+  wire ps, pc1, pc2;
+
+  xor #(2,3) (ps,  a,   b);
+  and #(2,3) (pc1, a,   b);
+  xor #(2,3) (sum, cin, ps);
+  and #(2,3) (pc2, cin, ps);
+  or #(2,3) (cout, pc1, pc2);
 
 endmodule
